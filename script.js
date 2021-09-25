faeabf79bd91303104f94058dec27ab7d3df5554
AdnanAhad <adnanahad.in@gmail.com>
2021-09-25 12:38:28 +0530

    now in normal form

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 console.log("fetch API");
 
+// Asynchronous
+// function getData() {
+//   console.log("inside function");
+//   url = "https://api.github.com/users";
+//   fetch(url)
+//     .then((response) => {
+//       console.log("inside response");
+
+//       return response.text();
+//     })
+//     .then((data) => {
+//       console.log("inside data");
+
+//       console.log(data);
+//     });
+// }
+
 function getData() {
-  console.log("inside function");
   url = "https://api.github.com/users";
   fetch(url)
     .then((response) => {
-      console.log("inside response");
-
       return response.text();
     })
     .then((data) => {
-      console.log("inside data");
-
       console.log(data);
     });
 }
 
-console.log("before fetching data");
-
 getData();
-
-console.log("after fetching Data");
