664cb00abf77571ec6afaabd79251f8f744b123b
AdnanAhad <adnanahad.in@gmail.com>
2021-09-25 12:37:40 +0530

    checking asynchronous behaviour of fetch

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 console.log("fetch API");
 
 function getData() {
-  url = "adu.txt";
+  console.log("inside function");
+  url = "https://api.github.com/users";
   fetch(url)
     .then((response) => {
+      console.log("inside response");
+
       return response.text();
     })
     .then((data) => {
+      console.log("inside data");
+
       console.log(data);
     });
 }
