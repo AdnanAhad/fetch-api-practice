971d5b3476964107e32d47c8f5adff62c2720479
AdnanAhad <adnanahad.in@gmail.com>
2021-09-25 11:27:49 +0530

    added text file and first fetch api call got data successfully

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("fetch API");
+
+function getData() {
+  url = "adu.txt";
+  fetch(url)
+    .then((response) => {
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+console.log("before fetching data");
+
+getData();
+
+console.log("after fetching Data");
